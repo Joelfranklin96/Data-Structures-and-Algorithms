73e7b2a0b487e09bc0edb2afd92b0847d28df150
S Joel Franklin <53593501+Joelfranklin96@users.noreply.github.com>
2021-03-07 21:35:37 +0530

    Update 1. daysBetweenDates.py

--- 1. daysBetweenDates.py
+++ 1. daysBetweenDates.py
@@ -2,7 +2,11 @@ not_leap = [31,28,31,30,31,30,31,31,30,31,30,31]
 leap = [31,29,31,30,31,30,31,31,30,31,30,31]
 
 def leap_year(year):
-    if year %4 == 0:
+    if year%400 == 0:
+        return True
+    elif year%100 == 0:
+        return False
+    elif year %4 == 0:
         return True
     else:
         return False
